4c66bbe710dcf89cd1554713e000de267e6a161c
akgamerpro20 <akgamerpro20@gmail.com>
2020-08-02 01:11:47 +0630

    Updates

---
+++ precache-manifest.ef584801d3d53f44d8662401efbbc263.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c04a2c12de23e39e51a9234d288bdd1",
+    "url": "/Burger-Order/index.html"
+  },
+  {
+    "revision": "beefb2f6bf188763a0a0",
+    "url": "/Burger-Order/static/css/main.316faa19.chunk.css"
+  },
+  {
+    "revision": "3522585242e17677d59d",
+    "url": "/Burger-Order/static/js/2.a8cace87.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Burger-Order/static/js/2.a8cace87.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "beefb2f6bf188763a0a0",
+    "url": "/Burger-Order/static/js/main.c4e971cd.chunk.js"
+  },
+  {
+    "revision": "49c964730bebec28d9c0",
+    "url": "/Burger-Order/static/js/runtime-main.5609581a.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/Burger-Order/static/media/burger-logo.b8503d26.png"
+  }
+]);
