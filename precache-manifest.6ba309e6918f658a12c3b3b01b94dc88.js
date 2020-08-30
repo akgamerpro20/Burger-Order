d7ecff7fd499341fce6c7fbb1eb0f1a205734f99
akgamerpro20 <akgamerpro20@gmail.com>
2020-08-30 21:14:51 +0630

    Updates

---
+++ precache-manifest.6ba309e6918f658a12c3b3b01b94dc88.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d979f6860796a09a1bedd643c485dcd",
+    "url": "/burger/index.html"
+  },
+  {
+    "revision": "bc2f6f66caaa5d7e15a1",
+    "url": "/burger/static/css/main.7788bcf6.chunk.css"
+  },
+  {
+    "revision": "3fb01e5e0e8b8d71efe1",
+    "url": "/burger/static/js/2.228f51a7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/burger/static/js/2.228f51a7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc2f6f66caaa5d7e15a1",
+    "url": "/burger/static/js/main.0a671250.chunk.js"
+  },
+  {
+    "revision": "368b43dacc6d02ba5b36",
+    "url": "/burger/static/js/runtime-main.43ad328d.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5c529c2e9126cfe0fa400bcaf23ed32e.js"
+  "/burger/precache-manifest.6ba309e6918f658a12c3b3b01b94dc88.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/burger/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
